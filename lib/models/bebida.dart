70021aab99f55a80c20ca8c02a410a75570245d6
Luana <luanafialho140@gmail.com>
2025-05-20 11:28:07 -0300

    Adicionando comentários e corrigindo erro

--- lib/models/bebida.dart
+++ lib/models/bebida.dart
@@ -11,30 +11,3 @@ class Bebida extends Produto {
     required this.preco,
   }) : super(nome: nome, imagem: imagem);
 }
-
-final List<Bebida> bebidas = [
-  Bebida(
-    nome: 'Coca-Cola 2 litros',
-    imagem: 'assets/imagens/coca2litros.jpg',
-    preco: 14.00,
-    volume: 2000,
-  ),
-  Bebida(
-    nome: 'Guaraná 2 litros',
-    imagem: 'assets/imagens/coca2litros.jpg',
-    preco: 12.00,
-    volume: 2000,
-  ),
-  Bebida(
-    nome: 'Cola-cola 600ml',
-    imagem: 'assets/imagens/coca2litros.jpg',
-    preco: 7.50,
-    volume: 600,
-  ),
-  Bebida(
-    nome: 'Guaraná 1 litro',
-    imagem: 'assets/imagens/coca2litros.jpg',
-    preco: 7.00,
-    volume: 1000,
-  ),
-];
