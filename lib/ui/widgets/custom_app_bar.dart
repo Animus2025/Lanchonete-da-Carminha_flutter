43c9c8999e8cdb0cf71e98024bb50984c2f2870e
João Henrique <jhaguiar1@outlook.com>
2025-06-21 15:43:28 -0300

    Update custom_app_bar.dart
    cor do menu ajustada

--- lib/ui/widgets/custom_app_bar.dart
+++ lib/ui/widgets/custom_app_bar.dart
@@ -55,44 +55,32 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           /// 🔥 Se está logado, mostra submenu; senão, botão que abre login
           auth.isLoggedIn
               ? PopupMenuButton<String>(
-                  icon: const Icon(
-                    Icons.person,
-                    size: 28,
-                    color: AppColors.laranja,
-                  ),
-                  color: Colors.white,
                   offset: const Offset(0, 48), // <-- faz o menu abrir para baixo
+                  icon: const Icon(Icons.person, color: AppColors.laranja),
+                  color: AppColors.preto, // Fundo preto
                   onSelected: (value) {
                     if (value == 'minha_conta') {
                       Navigator.of(context).pushNamed('/minha_conta');
                     } else if (value == 'sair') {
                       auth.logout();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Você saiu da conta!'),
-                          duration: Duration(seconds: 2),
-                        ),
-                      );
                     }
                   },
-                  itemBuilder: (context) {
-                    return [
-                      const PopupMenuItem(
-                        value: 'minha_conta',
-                        child: Text(
-                          'Minha Conta',
-                          style: TextStyle(color: Colors.black),
-                        ),
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      value: 'minha_conta',
+                      child: const Text(
+                        'Minha Conta',
+                        style: TextStyle(color: AppColors.laranja), // Letras laranjas
                       ),
-                      const PopupMenuItem(
-                        value: 'sair',
-                        child: Text(
-                          'Sair',
-                          style: TextStyle(color: Colors.black),
-                        ),
+                    ),
+                    PopupMenuItem(
+                      value: 'sair',
+                      child: const Text(
+                        'Sair',
+                        style: TextStyle(color: AppColors.laranja), // Letras laranjas
                       ),
-                    ];
-                  },
+                    ),
+                  ],
                 )
               : IconButton(
                   icon: const Icon(
